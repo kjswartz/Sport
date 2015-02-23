a3912ed23b36b4d099f1d878d3236f4626db27cf
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-23 15:08:15 -0500

    changed conditional arguements.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,16 @@ unless League.count > 0
   4.times do |l|
     League.create(name: "League #{l}", skill: Faker::Lorem.sentence)
   end
+end
 
+unless Team.count > 0
   rand_league_id = League.pluck(:id).shuffle[0]
   20.times do |t|
       Team.create(name: Faker::Company.name, league_id: rand_league_id)
   end
+end
 
+unless Player.counter > 0
   rand_team_id = Team.pluck(:id).shuffle[0]
   60.times do |p|
     Player.create(name: Faker::Name.name, team_id: rand_team_id)
