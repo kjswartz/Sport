0e6a763d2f9ae696b004f2562130d655e1ee0d66
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-23 14:02:47 -0500

    added faker gem and seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+unless League.count > 0
+  5.times do |l|
+    League.create(name: "League #{l}", skill: Faker::Lorem.words(5))
+      5.times do |t|
+        Team.create(name: Faker::Company.name, created_at: Faker::Date.between(30.days.ago, Date.today), league_id: t)
+          5.times do |p|
+            Player.create(name: Faker::Name.name, created_at: Faker::Date.between(30.days.ago, Date.today), team_id: p)
+          end
+      end
+  end
+end
