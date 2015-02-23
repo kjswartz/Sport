ebfe7c0cccda5e6a0fd202c013c6af72de2750ae
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-23 15:00:25 -0500

    created rand league and team ids.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,13 @@ unless League.count > 0
     League.create(name: "League #{l}", skill: Faker::Lorem.sentence)
   end
 
+  rand_league_id = League.pluck(:id).shuffle[0]
   20.times do |t|
-      Team.create(name: Faker::Company.name, league_id: rand(4))
+      Team.create(name: Faker::Company.name, league_id: rand_league_id)
   end
 
+  rand_team_id = Team.pluck(:id).shuffle[0]
   60.times do |p|
-    Player.create(name: Faker::Name.name, team_id: rand(20))
+    Player.create(name: Faker::Name.name, team_id: rand_team_id)
   end
 end
