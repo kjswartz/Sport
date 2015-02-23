4db13e85f8626916c2c0568bf06af1680419c7ea
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-23 15:49:26 -0500

    added .sample to id arrays.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,15 @@ unless League.count > 0
 end
 
 unless Team.count > 0
-  rand_league_id = League.pluck(:id).shuffle[0..4][0]
+  rand_league_id = League.pluck(:id).shuffle[0..4]
   20.times do |t|
-      Team.create(name: Faker::Company.name, league_id: rand_league_id)
+    Team.create(name: Faker::Company.name, league_id: rand_league_id.sample)
   end
 end
 
 unless Player.count > 0
   rand_team_id = Team.pluck(:id).shuffle[0..20][0]
   60.times do |p|
-    Player.create(name: Faker::Name.name, team_id: rand_team_id)
+    Player.create(name: Faker::Name.name, team_id: rand_team_id.sample)
   end
 end
