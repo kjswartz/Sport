eb31d976910a0d02fb740409c286ef64806d7e47
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-02-24 18:37:11 -0500

    fixed random team_ids assignment to payer object.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@ unless Team.count > 0
 end
 
 unless Player.count > 0
-  rand_team_id = Team.pluck(:id).shuffle[0..20]
+  rand_team_id = Team.pluck(:id).shuffle[0..12]
   40.times do |p|
-    Player.create(name: Faker::Name.name, team_ids: rand_team_id.sample, rand_team_id.sample)
+    Player.create(name: Faker::Name.name, team_ids: rand_team_id.sample(rand(3)))
   end
 end
